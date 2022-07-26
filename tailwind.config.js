e822620758888fce9b92d89b95aa55216266e5a5
Oğuzhan Arnak <arnakoguzhan.7@gmail.com>
2022-07-26 11:10:42 +0300

    Tailwind configuration updated to enable dynamic bg classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
+const tailwindColors = require("./node_modules/tailwindcss/colors");
+const colorSafeList = [];
+
+// Skip these to avoid a load of deprecated warnings when tailwind starts up
+const deprecated = [
+    "lightBlue",
+    "warmGray",
+    "trueGray",
+    "coolGray",
+    "blueGray",
+];
+
+for (const colorName in tailwindColors) {
+    if (deprecated.includes(colorName)) {
+        continue;
+    }
+
+    const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+    const pallette = tailwindColors[colorName];
+
+    if (typeof pallette === "object") {
+        shades.forEach((shade) => {
+            if (shade in pallette) {
+                colorSafeList.push(`text-${colorName}-${shade}`);
+                colorSafeList.push(`bg-${colorName}-${shade}`);
+            }
+        });
+    }
+}
+
 module.exports = {
+    safelist: colorSafeList,
     content: ["./resources/js/**/*.{vue,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: tailwindColors,
+        },
     },
     plugins: [],
 };
