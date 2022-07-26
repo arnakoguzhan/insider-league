f10d5d9adec55e75c67c8a5b697537dde0e082ac
Oğuzhan Arnak <arnakoguzhan.7@gmail.com>
2022-07-27 00:02:12 +0300

    Production build for frontend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-    <script src="{{ asset(mix('js/manifest.js')) }}" defer></script>
-    <script src="{{ asset(mix('js/vendor.js')) }}" defer></script>
-    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('js/manifest.js') }}" defer></script>
+    <script src="{{ mix('js/vendor.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     @inertiaHead
 </head>
 
